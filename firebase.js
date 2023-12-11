4b4c8fcaa9c77395ef4d2601c53f3b1fd38f28da
Ariana Guilak <arianaguilak@Arianas-Air.lan>
2023-12-11 11:15:49 -0800

    add firebase config

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection } from "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB9MyV2a9BvILBt75PVvf_-15BudIqcdjY",
+  authDomain: "mde-notes-app.firebaseapp.com",
+  projectId: "mde-notes-app",
+  storageBucket: "mde-notes-app.appspot.com",
+  messagingSenderId: "173448070002",
+  appId: "1:173448070002:web:fff078f44576bcebbd93eb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+const notesCollection = collection(db, "notes")
